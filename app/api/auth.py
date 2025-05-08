fa98c566fc53cb20080d94576b62833637874f52
Ahmed Safwat <ahmed-881994@hotmail.com>
2025-05-09 01:58:36 +0300

    Remove unused imports and clean up the OAuth router in auth.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,19 +1,6 @@
-import os
-from datetime import datetime
-
-from fastapi import APIRouter, Form, HTTPException
+from fastapi import APIRouter
 
 from app.schema.auth.auth import TokenResponse
-from app.service.auth import users
-from app.util.auth import compute_s256_challenge, create_access_token, generate_code
-
-auth_codes = {}
-clients = {
-    "client1": {
-        "client_secret": "secret1",
-        "redirect_uris": ["http://localhost:8000/docs/oauth2-redirect"],
-    }
-}
 
 router = APIRouter(prefix="/oauth", tags=["Auth"])
 
