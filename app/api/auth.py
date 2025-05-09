3f06c2c3bd3996cbdcc1eb4276aa71e1cb190be3
Ahmed Safwat <ahmed-881994@hotmail.com>
2025-05-09 15:41:33 +0300

    Refactor get_token function to include error handling for MySQLError and improve code structure

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,6 +4,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from pymysql import MySQLError
 
 from app.schema.auth.Token import Token
 from app.service.auth.users import authenticate_user
@@ -14,15 +15,18 @@ router = APIRouter(tags=["Auth"])
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("access_token_expires_minutes", 30))
 @router.post("/token", response_model=Token)
 def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
+    try:
+        user = authenticate_user(form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.user_name}, expires_delta=access_token_expires
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.user_name}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+        return Token(access_token=access_token, token_type="bearer")
+    except MySQLError as error:
+        raise HTTPException(status_code=500, detail=error.args)
