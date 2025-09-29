82f1e3b6f07d32e0a7d720b7969bbd04676fa0a7
Ahmed Safwat <ahmed-881994@hotmail.com>
2025-09-29 20:11:23 +0300

    Add additional member fields to create_member method for enhanced member data capture

--- app/repositories/member_repository.py
+++ app/repositories/member_repository.py
@@ -45,7 +45,47 @@ class MemberRepository(BaseRepository[Member]):
 
         try:
             # Create member instance
-            member = Member(**member_data)
+            member = Member()
+            member.member_id = member_data.get("member_id")
+            member.name_en = member_data.get("name_en")
+            member.name_ar = member_data.get("name_ar")
+            member.place_of_birth = member_data.get("place_of_birth")
+            member.date_of_birth = member_data.get("date_of_birth")
+            member.address = member_data.get("address")
+            member.national_id_no = member_data.get("national_id_no")
+            member.club_id_no = member_data.get("club_id_no")
+            member.passport_no = member_data.get("passport_no")
+            member.date_joined = member_data.get("date_joined")
+            member.mobile_number = member_data.get("mobile_number")
+            member.home_contact = member_data.get("home_contact")
+            member.email = member_data.get("email")
+            member.facebook_url = member_data.get("facebook_url")
+            member.school_name = member_data.get("school_name")
+            member.education_type = member_data.get("education_type")
+            member.father_name = member_data.get("father_name")
+            member.father_contact = member_data.get("father_contact")
+            member.father_job = member_data.get("father_job")
+            member.mother_name = member_data.get("mother_name")
+            member.mother_contact = member_data.get("mother_contact")
+            member.mother_job = member_data.get("mother_job")
+            member.guardian_name = member_data.get("guardian_name")
+            member.guardian_contact = member_data.get("guardian_contact")
+            member.guardian_relationship = member_data.get("guardian_relationship")
+            member.hobbies = member_data.get("hobbies")
+            member.health_issues = member_data.get("health_issues")
+            member.medications = member_data.get("medications")
+            member.qr_code_url = member_data.get("qr_code_url")
+            member.image_url = member_data.get("image_url")
+            member.national_id_url = member_data.get("national_id_url")
+            member.parent_national_id_url = member_data.get("parent_national_id_url")
+            member.club_id_url = member_data.get("club_id_url")
+            member.passport_url = member_data.get("passport_url")
+            member.birth_certificate_url = member_data.get("birth_certificate_url")
+            member.photo_consent = member_data.get("photo_consent")
+            member.conditions_consent = member_data.get("conditions_consent")
+            
+            
+            
             
             # Use base repository create method
             return super().create(member)
